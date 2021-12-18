119ae0e28770f32091bbe6400ba76f3e9ca99976
zhaozhejun <ybbos@qq.com>
2021-12-18 23:53:55 +0800

    Update brook-pf-mod.sh

--- brook-pf-mod.sh
+++ brook-pf-mod.sh
@@ -138,9 +138,9 @@ Download_brook(){
     [[ ! -e ${file} ]] && mkdir ${file}
     cd ${file}
     if [[ ${bit} == "x86_64" ]]; then
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_amd64" && mv brook_linux_amd64 brook
+        wget --no-check-certificate -N "https://ghproxy.com/https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_amd64" && mv brook_linux_amd64 brook
     else
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+        wget --no-check-certificate -N "https://ghproxy.com/https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
         mv brook_linux_386 brook
     fi
     [[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
@@ -148,14 +148,14 @@ Download_brook(){
 }
 Service_brook(){
     if [[ ${release} = "centos" ]]; then
-        if ! wget --no-check-certificate https://raw.githubusercontent.com/ECIAP/brook/master/brook-pf_centos -O /etc/init.d/brook-pf; then
+        if ! wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/ECIAP/brook/master/brook-pf_centos -O /etc/init.d/brook-pf; then
             echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
         fi
         chmod +x /etc/init.d/brook-pf
         chkconfig --add brook-pf
         chkconfig brook-pf on
     else
-        if ! wget --no-check-certificate https://raw.githubusercontent.com/ECIAP/brook/master/brook-pf_debian -O /etc/init.d/brook-pf; then
+        if ! wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/ECIAP/brook/master/brook-pf_debian -O /etc/init.d/brook-pf; then
             echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
         fi
         chmod +x /etc/init.d/brook-pf
